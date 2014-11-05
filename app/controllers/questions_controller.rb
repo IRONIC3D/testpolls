048832f0ce053069d5ef1ba5142b2ace30bf6700
Iyad Horani <i3d@ironic3d.com>
2014-11-06 10:42:37 +1100

    Add appropriate relations in questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :set_poll
+  before_action :set_kind_questions
 
   def index
     @questions = Question.all
@@ -10,18 +11,18 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
+    @question = @poll.questions.build
   end
 
   def edit
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = @poll.questions.build(question_params)
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
+        format.html { redirect_to @poll, notice: 'Question was successfully created.' }
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new }
@@ -45,7 +46,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+      format.html { redirect_to @poll, notice: 'Question was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -61,5 +62,12 @@ class QuestionsController < ApplicationController
 
     def set_poll
       @poll = Poll.find params[:poll_id]
+    end
+
+    def set_kind_questions
+      @kind_options = [
+        ["Open Answer", "open"],
+        ["Multiple Choice", "choice"]
+      ]
     end
 end
